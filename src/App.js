f64c2591005a54f205b6441dab012af8240e329a
gtretow <guilherme.tretow@gmail.com>
2021-01-16 21:32:16 -0300

    first test pass

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from "react";
 import "./App.css";
 
-import GuessedWords from "./GuessedWords";
-import Congrats from "./Congrats";
-
 function App() {
-  return (
-    <div className="container">
-      {/* hard code pois não temos a parte do Redux/hooks ainda */}
-      <h1>The Jotto Game!</h1>
-      <Congrats success={true} />
-      <GuessedWords guessedWords={[{guessedWord: 'train', letterMatchCount: 3}]} />
-
-    </div>
-  );
+  return <div data-test="component-app"></div>;
 }
 
 export default App;

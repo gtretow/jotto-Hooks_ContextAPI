c506fbfcd18e5960dc2cf1ac05f2241041bcd7d2
gtretow <guilherme.tretow@gmail.com>
2021-01-16 22:27:20 -0300

    state updates with value of input box upon change

--- src/Input.js
+++ src/Input.js
@@ -1,8 +1,35 @@
 import React from "react";
-import "./App.css";
+import propTypes from "prop-types";
 
-function Input() {
-  return <div data-test="component-input"></div>;
+//destruturação de secretWord
+function Input({ secretWord }) {
+  const [currentGuess, setCurrentGuess] = React.useState("");
+  // é necessário retornar o state assim para usar mock functions
+
+  return (
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-s mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+          value={currentGuess}
+          onChange={(event) => setCurrentGuess(event.target.value)}
+        ></input>
+        <button data-test="submit-button" className="btn btn-primary mb-2">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
+
+//uso de mocks
+//Não deixa funções reais rodarem, preve network calls pode espiar função para ver qnd é chamada (assim vc sabe que ela é chamada na hora certa e com argumentos certos) e vc também pode providenciar return values
+
+Input.propTypes = {
+  secretWord: propTypes.string.isRequired,
+};
 
 export default Input;
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,17 +1,37 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Input from "./Input";
-import { findByTestAttr } from "../test/testUtils";
+import { checkProps, findByTestAttr } from "../test/testUtils";
 
-const setup = () => {
-  return shallow(<Input />);
+const setup = (secretWord = "party") => {
+  return shallow(<Input secretWord={secretWord} />);
 };
 
-test('Input renders without errors', () => {
-    
-const wrapper = setup();
-const component = findByTestAttr(wrapper, "component-input")
-expect(component.length).toBe(1)
+test("Input renders without errors", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-input");
+  expect(component.length).toBe(1);
+});
 
+test("does not throw warning with expected props", () => {
+  checkProps(Input, { secretWord: "party" });
+});
 
-})
+//não desestruturar useContext no import
+//import React from 'react'; é o correto para testes, não import {useContext}
+
+describe("state controller input field", () => {
+  test("state updates with value of input box upon change", () => {
+    const mockSetCurrentGuess = jest.fn();
+    React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+
+    const wrapper = setup();
+    const inputBox = findByTestAttr(wrapper, "input-box");
+
+    //simulando inputbox recebendo um value de "train"
+    const mockEvent = { target: { value: "train" } };
+    inputBox.simulate("change", mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
+  });
+});
